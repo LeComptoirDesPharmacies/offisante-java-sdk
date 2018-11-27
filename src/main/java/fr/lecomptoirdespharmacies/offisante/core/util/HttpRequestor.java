e589e48f505b63f5e03274cdb04390f5d479b384
Géry Thrasibule <gery@thrasibule.mx>
2018-11-27 17:00:39 +0100

    📮 : Set custom time out for http request
    Create and set custom time for ok http client
    
    Signed-off-by: Géry Thrasibule <gery@thrasibule.mx>

--- src/main/java/fr/lecomptoirdespharmacies/offisante/core/util/HttpRequestor.java
+++ src/main/java/fr/lecomptoirdespharmacies/offisante/core/util/HttpRequestor.java
@@ -3,6 +3,7 @@ package fr.lecomptoirdespharmacies.offisante.core.util;
 import okhttp3.*;
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Class who execute HTTP request
@@ -11,9 +12,18 @@ public class HttpRequestor implements HttpRequest {
 
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
-    private OkHttpClient client = new OkHttpClient();
+    private static final Integer CONNECT_TIMEOUT = 30;
+    private static final Integer WRITE_TIMEOUT = 30;
+    private static final Integer READ_TIMEOUT = 60;
 
-    //TODO: better management of error
+    private OkHttpClient client = new OkHttpClient
+            .Builder()
+            .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            .build();
+
+
     @Override
     public String post(String url, String body, Map<String, String> header){
         // Set Body
@@ -33,14 +43,15 @@ public class HttpRequestor implements HttpRequest {
         Request request = rBuilder.build();
 
         // Handle exception
-        try {
-            Response response = client.newCall(request).execute();
+        try (Response response = client.newCall(request).execute()) {
             return response.body().string();
         } catch (NullPointerException e){
             // Body is empty
+            e.printStackTrace();
             throw new IllegalArgumentException();
         } catch (IOException ie){
             // IO Exception
+            ie.printStackTrace();
             throw new RuntimeException();
         }
     }
