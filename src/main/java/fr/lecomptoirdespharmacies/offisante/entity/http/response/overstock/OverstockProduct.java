c20f8b418b0002a9f2238d14e76f1a3ad56cc0c5
Géry THRASIBULE <g.thrasibule@lecomptoirdespharmacies.fr>
2021-10-28 18:23:12 +0200

    add last sale to Overstock product
    Envoyé depuis mon iPhone.
    P.S. : Ce commit est certifié sans gluten

--- src/main/java/fr/lecomptoirdespharmacies/offisante/entity/http/response/overstock/OverstockProduct.java
+++ src/main/java/fr/lecomptoirdespharmacies/offisante/entity/http/response/overstock/OverstockProduct.java
@@ -2,10 +2,13 @@ package fr.lecomptoirdespharmacies.offisante.entity.http.response.overstock;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import fr.lecomptoirdespharmacies.offisante.core.json.deserializer.ProductDateTimeDeserializer;
 import fr.lecomptoirdespharmacies.offisante.entity.http.response.ResponseProduct;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.joda.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -16,4 +19,7 @@ public class OverstockProduct extends ResponseProduct {
     Integer forecast;
     @JsonProperty("unsold")
     Integer unsold;
+    @JsonProperty("last_sale")
+    @JsonDeserialize(using = ProductDateTimeDeserializer.class)
+    LocalDateTime lastSale;
 }
