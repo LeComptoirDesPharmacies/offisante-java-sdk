6696e2fac35ccccadd510b21b3302b452dea7670
Thomas <thomas@lecomptoirdespharmacies.fr>
2026-02-26 12:45:39 +0100

    LDS-5466: fix lastDateStock deserializer

--- src/main/java/fr/lecomptoirdespharmacies/offisante/entity/Pharmacy.java
+++ src/main/java/fr/lecomptoirdespharmacies/offisante/entity/Pharmacy.java
@@ -3,6 +3,8 @@ package fr.lecomptoirdespharmacies.offisante.entity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import fr.lecomptoirdespharmacies.offisante.core.json.deserializer.LocalDateDeserializer;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -20,5 +22,6 @@ public class Pharmacy extends BaseEntity {
     String cip;
 
     @JsonProperty("last_date_stock")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     LocalDate lastDateStock;
 }
